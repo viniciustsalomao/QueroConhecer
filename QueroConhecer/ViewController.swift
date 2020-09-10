4d88fd3d71fff1603931d9433104cc661f772e5c
Vinícius Tinajero Salomão <viniciustsalomao@hotmail.com>
2020-09-09 21:54:12 -0300

    Initial Commit

---
+++ QueroConhecer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QueroConhecer
+//
+//  Created by Vinícius Tinajero Salomão on 09/09/20.
+//  Copyright © 2020 Vinícius Tinajero Salomão. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
